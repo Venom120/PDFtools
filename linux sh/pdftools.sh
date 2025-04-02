d6c4ee36a4d059d6fca2601d7f16f1a26ba20047
Venom120 <yatharthjain120@gmail.com>
2025-04-02 14:54:39 +0530

    file management improved

---
+++ linux sh/pdftools.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# move this file to /usr/bin/pdf.sh
+# (and give executable permissions)
+# chmod u+x /usr/bin/pdf.sh
+
+source /mnt/2050F97550F95250/Github/PDFtools/.venv/bin/activate
+
+python /mnt/2050F97550F95250/Github/PDFtools/pdftools.py "$@"
---
+++ windows bat/pdftools.bat
@@ -0,0 +1,2 @@
+@echo off
+python "D:\PATH\pdftools.py" %*
